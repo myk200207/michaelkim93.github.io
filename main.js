484cedc6c15d7ee30cdc664c397fd71381500313
myk200207 <138054037+myk200207@users.noreply.github.com>
2023-07-06 15:35:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Buspopup(){
+    document.getElementById("popup1").classList.toggle("active");
+}
+function Govpopup(){
+    document.getElementById("popup2").classList.toggle("active");
+}
+function wordpopup(){
+    document.getElementById("popup3").classList.toggle("active");
+}
+function pagepopup(){
+    document.getElementById("popup4").classList.toggle("active");
+}
+function sigpopup(){
+    document.getElementById("popup5").classList.toggle("active");
+}
